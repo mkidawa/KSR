0bf67f4db6b104d4d1ded6c9cab0c9ac1f33cb54
Aleksandra <aleksandrawnuk0@gmail.com>
2020-03-29 17:26:22 +0200

    Add Chebyshev Metric

---
+++ Zad1/src/main/java/org/awmk/ksr1/metrics/ChebyshevMetric.java
@@ -0,0 +1,16 @@
+package org.awmk.ksr1.metrics;
+
+import java.util.List;
+
+public class ChebyshevMetric implements Metric {
+    public float calculateDistance(List<Float> first, List<Float> second) {
+        float maxDistance = 0;
+        for (int i = 0; i < first.size(); i++) {
+            float distance = Math.abs(first.get(i) - second.get(i));
+            if (distance > maxDistance) {
+                maxDistance = distance;
+            }
+        }
+        return maxDistance;
+    }
+}
