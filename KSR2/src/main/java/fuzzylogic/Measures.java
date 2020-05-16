8aa10eeff73edc53e1de9057d4cb829520f2c244
Aleksandra <aleksandrawnuk0@gmail.com>
2020-05-16 20:34:31 +0200

    Add summaries with qualifiers

--- KSR2/src/main/java/fuzzylogic/Measures.java
+++ KSR2/src/main/java/fuzzylogic/Measures.java
@@ -3,18 +3,27 @@ package fuzzylogic;
 import java.util.List;
 
 public class Measures<T> {
-    public double degreeOfTruth(Quantifier<T> quantifier, LinguisticVariable<T> summarizer, List<T> objects) {
+    public double degreeOfTruth(Quantifier<T> quantifier, LinguisticVariable<T> qualifier, LinguisticVariable<T> summarizer, List<T> objects) {
         double r = 0;
+        double m = 0;
 
-        for(T obj : objects) {
-            r += summarizer.getMembership(obj);
-        }
-
-        if(quantifier.isAbsolute()) {
-            return quantifier.getSet().getMembershipFunction().getMembership(r);
+        if (qualifier == null) { // summary without qualifier
+            for (T obj : objects) {
+                r += summarizer.getMembership(obj);
+            }
+            m = objects.size();
         } else {
-            return quantifier.getSet().getMembershipFunction().getMembership(r / objects.size());
+            for (T obj : objects) {
+                r += Math.min(summarizer.getMembership(obj), qualifier.getMembership(obj));
+                m += qualifier.getMembership(obj);
+            }
         }
+
+        if (quantifier.isAbsolute() && qualifier == null) {
+            return quantifier.getSet().getMembershipFunction().getMembership(r);
+        } else if (!quantifier.isAbsolute()) {
+            return quantifier.getSet().getMembershipFunction().getMembership(r / m);
+        } else return 0.0;
     }
 
 }
