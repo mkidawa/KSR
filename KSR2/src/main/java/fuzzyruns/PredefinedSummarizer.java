f81a2b86f59415b47d28e0ccfc3b797af24ffa6e
Aleksandra <aleksandrawnuk0@gmail.com>
2020-05-18 20:32:21 +0200

    Add new summarizers

--- KSR2/src/main/java/fuzzyruns/PredefinedSummarizer.java
+++ KSR2/src/main/java/fuzzyruns/PredefinedSummarizer.java
@@ -18,6 +18,11 @@ public class PredefinedSummarizer {
 
     private static Function<RunDao, Double> ageFunc = run -> (double) run.getHorseAge();
     private static Function<RunDao, Double> declaredWeightFunc = RunDao::getDeclaredWeight;
+    private static Function<RunDao, Double> ratingFunc = run -> (double) run.getHorseRating();
+    private static Function<RunDao, Double> finishTimeFunc = RunDao::getFinishTime;
+    private static Function<RunDao, Double> winOddsFun = RunDao::getWinOdds;
+    private static Function<RunDao, Double> lengthsBehindFunc = RunDao::getLengthsBehind;
+    private static Function<RunDao, Double> actualWeightFunc = RunDao::getActualWeight;
 
     public PredefinedSummarizer() {
         summarizers = new ArrayList<>();
@@ -74,6 +79,137 @@ public class PredefinedSummarizer {
             "very heavy",
             new TrapezoidalFunction(1250.0, 1396.0, 1350.0, 1396.0),
             declaredWeightFunc
+    );
+
+    public static LinguisticVariable<RunDao> ratingLow = new LinguisticVariable<>(
+            "horse rating",
+            "low rating",
+            new TrapezoidalFunction(10.0, 40.0, 10.0, 30.0),
+            ratingFunc
+    );
+    public static LinguisticVariable<RunDao> ratingMedium = new LinguisticVariable<>(
+            "horse rating",
+            "medium rating",
+            new TrapezoidalFunction(30.0, 90.0, 50.0, 70.0),
+            ratingFunc
+    );
+    public static LinguisticVariable<RunDao> ratingHigh = new LinguisticVariable<>(
+            "horse rating",
+            "high rating",
+            new TrapezoidalFunction(80.0, 138.0, 110.0, 138.0),
+            ratingFunc
+    );
+
+    public static LinguisticVariable<RunDao> finishTimeVeryFast = new LinguisticVariable<>(
+            "finish time",
+            "very fast",
+            new TrapezoidalFunction(55.16, 70.0, 55.16, 65.0),
+            finishTimeFunc
+    );
+    public static LinguisticVariable<RunDao> finishTimeFast = new LinguisticVariable<>(
+            "finish time",
+            "fast",
+            new TrapezoidalFunction(65.0, 90.0, 70.0, 80.0),
+            finishTimeFunc
+    );
+    public static LinguisticVariable<RunDao> finishTimeMedium = new LinguisticVariable<>(
+            "finish time",
+            "medium",
+            new TrapezoidalFunction(80.0, 120.0, 90.0, 110.0),
+            finishTimeFunc
+    );
+    public static LinguisticVariable<RunDao> finishTimeSlow = new LinguisticVariable<>(
+            "finish time",
+            "slow",
+            new TrapezoidalFunction(110.0, 150.0, 130.0, 140.0),
+            finishTimeFunc
+    );
+    public static LinguisticVariable<RunDao> finishTimeVerySlow = new LinguisticVariable<>(
+            "finish time",
+            "very slow",
+            new TrapezoidalFunction(140.0, 163.58, 150.0, 163.58),
+            finishTimeFunc
+    );
+
+    public static LinguisticVariable<RunDao> winOddsLow = new LinguisticVariable<>(
+            "win odds",
+            "low odds",
+            new TrapezoidalFunction(1.0, 40.0, 1.0, 20.0),
+            winOddsFun
+    );
+    public static LinguisticVariable<RunDao> winOddsMedium = new LinguisticVariable<>(
+            "win odds",
+            "medium odds",
+            new TrapezoidalFunction(20.0, 80.0, 40.0, 720.0),
+            winOddsFun
+    );
+    public static LinguisticVariable<RunDao> winOddsHigh = new LinguisticVariable<>(
+            "win odds",
+            "high odds",
+            new TrapezoidalFunction(70.0, 99.9, 90.0, 99.9),
+            winOddsFun
+    );
+
+    public static LinguisticVariable<RunDao> lengthsBehindVeryFew = new LinguisticVariable<>(
+            "lengths behind winner",
+            "very few",
+            new TrapezoidalFunction(-0.5, 3.5, -0.5, 2.0),
+            lengthsBehindFunc
+    );
+    public static LinguisticVariable<RunDao> lengthsBehindFew = new LinguisticVariable<>(
+            "lengths behind winner",
+            "few",
+            new TrapezoidalFunction(1.5, 6.0, 3.0, 5.0),
+            lengthsBehindFunc
+    );
+    public static LinguisticVariable<RunDao> lengthsBehindSome = new LinguisticVariable<>(
+            "lengths behind winner",
+            "some",
+            new TrapezoidalFunction(3.0, 10.0, 4.0, 7.0),
+            lengthsBehindFunc
+    );
+    public static LinguisticVariable<RunDao> lengthsBehindMany = new LinguisticVariable<>(
+            "lengths behind winner",
+            "many",
+            new TrapezoidalFunction(7.0, 50.0, 10.0, 30.0),
+            lengthsBehindFunc
+    );
+    public static LinguisticVariable<RunDao> lengthsBehindVeryMany = new LinguisticVariable<>(
+            "lengths behind winner",
+            "very many",
+            new TrapezoidalFunction(35.0, 206.75, 50.0, 206.75),
+            lengthsBehindFunc
+    );
+
+    public static LinguisticVariable<RunDao> actualWeightVeryLight = new LinguisticVariable<>(
+            "actual weight",
+            "very light",
+            new TriangularFunction(103.0, 109.0, 103.0),
+            actualWeightFunc
+    );
+    public static LinguisticVariable<RunDao> actualWeightLight = new LinguisticVariable<>(
+            "actual weight",
+            "light",
+            new TriangularFunction(103.0, 115.0, 109.0),
+            actualWeightFunc
+    );
+    public static LinguisticVariable<RunDao> actualWeightMedium = new LinguisticVariable<>(
+            "actual weight",
+            "medium",
+            new TriangularFunction(109.0, 123.0, 115.0),
+            actualWeightFunc
+    );
+    public static LinguisticVariable<RunDao> actualWeightHeavy = new LinguisticVariable<>(
+            "actual weight",
+            "heavy",
+            new TriangularFunction(115.0, 127.0, 121.0),
+            actualWeightFunc
+    );
+    public static LinguisticVariable<RunDao> actualWeightVeryHeavy = new LinguisticVariable<>(
+            "actual weight",
+            "very heavy",
+            new TrapezoidalFunction(121.0, 133.0, 127.0, 133.0),
+            actualWeightFunc
     );
 
 }
