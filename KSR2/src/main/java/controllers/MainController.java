270ecca3dfd843798732771b46ee7f09519e75d3
mkidawa <mkidawa1@gmail.com>
2020-05-25 01:02:05 +0200

    Fix extra spaces

--- KSR2/src/main/java/controllers/MainController.java
+++ KSR2/src/main/java/controllers/MainController.java
@@ -199,11 +199,12 @@ public class MainController {
             }
             quantifier[i] = model.quantifier.getQuantifiers().get(i).getLinguisticVariableName();
             for (int j = 0; j < nrOfCurrentComboBoxes; j++){
-                String connective = j > 0 ? " and " : " were with ";
+                String connective = j > 0 ? " and " : "were with ";
                 text +=  connective + model.summarizers.get(j).getLinguisticVariableName() + " " + model.summarizers.get(j).getLabelName();
                 quantValues[0][j] = model.summarizers.get(j).getLinguisticVariableName() + " " + model.summarizers.get(j).getLabelName();
             }
-            text += "\n [T1 = " + T1 + ", T2 = " + T2 + ", T3 = " + T3 + ", T4 = " + T4 + ", T5 = " + T5 + ", T6 = " + T6 + ", T7 = " + T7 + ", T8 = " + T8 + ", T9 = " + T9 + ", T10 = " + T10 + ", T11 = " + T11 + "]. \n";
+            text += ".\n";
+            // text += "\n [T1 = " + T1 + ", T2 = " + T2 + ", T3 = " + T3 + ", T4 = " + T4 + ", T5 = " + T5 + ", T6 = " + T6 + ", T7 = " + T7 + ", T8 = " + T8 + ", T9 = " + T9 + ", T10 = " + T10 + ", T11 = " + T11 + "]. \n";
 
         }
         for (int j = 0; j < nrOfCurrentComboBoxes; j++){
